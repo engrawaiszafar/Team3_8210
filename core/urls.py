a118d24ccd2a939fdf46b5b43a3833e108d29dc1
Alekhya Gurram <ayalate@unomaha.edu>
2026-04-01 22:26:29 -0500

    update admin login and logout page

--- core/urls.py
+++ core/urls.py
@@ -5,4 +5,9 @@ from main import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+
+    path('admin-login/', views.admin_login_view, name='admin_login'),
+    path('admin-home/', views.admin_home, name='admin_home'),
+    path('logout-confirm/', views.logout_confirm, name='logout_confirm'),
+    path('logout/', views.logout_view, name='logout'),
 ]
